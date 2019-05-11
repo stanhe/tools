f4be6cb46e10b5fa9570edf8a4c2412d627825bb
stanhe <hshl4314@gmail.com>
2019-05-11 12:20:52 +0800

    d0511

--- sh/.screencap.sh
+++ sh/.screencap.sh
@@ -2,9 +2,24 @@
 #author: stanhe
 #usage: adb 截屏并保存到当前文件夹下
 
-INDEX=`adb shell ls mnt/sdcard/screencaps |wc -l`
-NAME="pic$INDEX.png"
-LOCAL_PATH="mnt/sdcard/screencaps/$NAME"
-`adb shell screencap -p $LOCAL_PATH`
-echo "screencap : $LOCAL_PATH"
-`adb pull $LOCAL_PATH $PWD/$NAME`
+createFolder(){
+    FOLDER=`adb shell ls mnt/sdcard | grep screencaps`
+    if [ ! $FOLDER ];
+    then
+	adb shell mkdir mnt/sdcard/screencaps
+	echo "===> mkdir screencaps!"
+    fi
+}
+startCap(){
+    INDEX=`adb shell ls mnt/sdcard/screencaps |wc -l`
+    NAME="pic$INDEX.png"
+    LOCAL_PATH="mnt/sdcard/screencaps/$NAME"
+    `adb shell screencap -p $LOCAL_PATH`
+    echo "===> get : $LOCAL_PATH"
+    $(adb pull $LOCAL_PATH $PWD/$NAME)
+}
+
+# check interval <200ms，screencaps take 1~2s,so check every time also acceptability
+
+createFolder
+startCap
