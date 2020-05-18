ef971b75c9a289526c7213a65675ce1cf5c7d562
stanhe <hshl4314@gmail.com>
2020-05-19 01:04:02 +0800

    adjust mcd

--- mcd.sh
+++ mcd.sh
@@ -7,12 +7,14 @@
 # 3 "..path3"
 
 MCD_PATH="C:/Users/`whoami`/tools/.mcd_path"
-if [ -z "$1" ];
-then
-    echo -e "===> list custom path : $MCD_PATH"
-    cat $MCD_PATH
-    echo -e "\n===> how to use? \n \"mcd 1\" to use dir 1 \n \"mcdr\" change to current dir! \n \"mcdt\" to change to today's test dir! \n \"start .\" to open current dir!"
-else
-    CPATH=`awk '{if (NR=="'$1'") print $2}' $MCD_PATH`
-    alias mcdr="cd $CPATH"
+arr=()
+if [ -e $MCD_PATH ];then
+echo -e "===> list custom path : $MCD_PATH"
+cat $MCD_PATH
+echo -e "\n===> how to use? \n \".1\" to use dir 1 \n \"mcdt\" to change to today's test dir! \n \"start .\" to open current dir!"
+mapfile -t arr < $MCD_PATH # file lines to array
+for((i=0;i<=${#arr[@]};i++));do
+    strArray=(${arr[i]}) #string to stringArray
+    alias ".${strArray[0]}"="cd ${strArray[1]}"
+done
 fi
